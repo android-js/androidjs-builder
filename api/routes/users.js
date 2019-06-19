1cd99adfb401658b08581aa765cb8b419c63054b
DeveshPankaj <pankajdevesh3@gmail.com>
2019-06-19 17:11:05 +0530

    Sync

--- api/routes/users.js
+++ api/routes/users.js
@@ -50,7 +50,7 @@ module.exports = function(app) {
         user_controller.get_user_record_by_id(req, res, req.query.id, year, month);
     })
 
-    app.route('user-login').get(function(req,res){
+    app.route('/user-login').get(function(req,res){
         user_controller.get_user_by_email_and_password(req, res, req.query.email, req.query.password);
     })
 }
