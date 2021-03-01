e119cb187b6c106ddc337d2f534ebc56293495b8
Fabio Manganiello <info@fabiomanganiello.com>
2021-03-01 12:28:54 +0100

    - Be more resilient if process.stdout is not available (e.g. if the process runs in a non-interactive script).
    - bin.js should actually be in dist/bin.js, not dist/src/bin.js (fixes ENOENT error on npm install -g).

--- src/modules/Html/ProgressBar.ts
+++ src/modules/Html/ProgressBar.ts
@@ -39,13 +39,16 @@ export class LoadingBar{
         let _left = this.currentIndex - 1;
         _left = _left < 0 ? 0 : _left;
         let _right = this.bar_length - _left - 1;
-        process.stdout.write(`${this.message}${this.chunksDownloaded} :` + chalk.green(` ${this.empty.repeat(_left)}${this.fill}${this.empty.repeat(_right)}`));
+        if (process.stdout.write)
+            process.stdout.write(`${this.message}${this.chunksDownloaded} :` + chalk.green(` ${this.empty.repeat(_left)}${this.fill}${this.empty.repeat(_right)}`));
     }
     clear(){
-        //@ts-ignore
-        process.stdout.clearLine();
-        //@ts-ignore
-        process.stdout.cursorTo(0);
+        if (process.stdout.clearLine)
+            //@ts-ignore
+            process.stdout.clearLine();
+        if (process.stdout.cursorTo)
+            //@ts-ignore
+            process.stdout.cursorTo(0);
     }
 
     start(speed?:number) {
@@ -83,7 +86,7 @@ export class ProgressBar {
     constructor(total?:number) {
         this.total = total || null;
         this.current = 0;
-        this.bar_length = process.stdout.columns - 30;
+        this.bar_length = process.stdout.columns ? process.stdout.columns - 30 : 0;
         // this.bar_length = 50;
     }
     next(progress) {
@@ -96,13 +99,16 @@ export class ProgressBar {
 
         this.clearLine();
         const _str = `  ${_p}% : ${chalk.green(this.bar_fill.repeat(_left-1)+_in_progress_code)}${ this.barr_empty.repeat(_right)}`;
-        process.stdout.write(_str);
+        if (process.stdout.write)
+            process.stdout.write(_str);
         // console.log(_left, _right)
     }
     clearLine() {
-        //@ts-ignore
-        process.stdout.clearLine();
-        //@ts-ignore
-        process.stdout.cursorTo(0);
+        if (process.stdout.clearLine)
+            //@ts-ignore
+            process.stdout.clearLine();
+        if (process.stdout.cursorTo)
+            //@ts-ignore
+            process.stdout.cursorTo(0);
     }
 }
